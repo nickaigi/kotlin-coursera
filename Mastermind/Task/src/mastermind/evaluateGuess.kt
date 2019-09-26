51632a182006b606f60dd3d4fd6de93a08a28d07
Nickson Kaigi <nickaigi@gmail.com>
2019-09-26 20:35:58 +0300

    mastermind

---
+++ Mastermind/Task/src/mastermind/evaluateGuess.kt
@@ -0,0 +1,57 @@
+package mastermind
+
+data class Evaluation(val rightPosition: Int, val wrongPosition: Int)
+
+fun evaluateGuess(secret: String, guess: String): Evaluation {
+    var right: Int = 0
+    for (i in 0..3){
+        if (secret[i] == guess[i]){
+            right++
+        }
+    }
+    val wrong = wrongPositions(secret, guess)
+    return Evaluation(right, wrong)
+}
+
+private fun wrongPositions(secret: String, guess: String): Int {
+    var letters = 0
+    var newSecret = ""
+    var newGuess = ""
+
+    for(i in 0..3) {
+        if(secret[i] != guess[i]) {
+            newSecret += secret[i]
+            newGuess += guess[i]
+        }
+    }
+
+    val foundLetters = mutableListOf<Char>()
+
+    if(newSecret.isNotEmpty()) {
+        for (element in guess) {
+            if (!foundLetters.contains(element)) {
+                val howManyInSecret = countRepetitionsOf(element, newSecret)
+                val howManyInGuess = countRepetitionsOf(element, newGuess)
+                if (howManyInSecret == howManyInGuess || howManyInSecret > howManyInGuess){
+                    letters += howManyInGuess
+                } else{
+                    letters += howManyInSecret
+                }
+
+                foundLetters.add(element)
+            }
+        }
+    }
+
+    return letters
+}
+
+fun countRepetitionsOf(value: Char, word: String): Int {
+    var count = 0
+    for (element in word) {
+        if (element == value) {
+            count++
+        }
+    }
+    return count
+}
